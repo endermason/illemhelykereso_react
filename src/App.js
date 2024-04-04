2754dead157789c54a1c45d778184a9318b88406
Bence Szabó <theendermason@gmail.com>
2024-04-05 01:07:58 +0200

    Signup, Bootsrtap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
 import { Auth } from "./components/auth";
+import Signup from "./components/signup";
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/dashboard";
 
 function App() {
   return (
-  <div className="App">
-    <Auth />
+  //<div className="App">
+    //<Auth />
+    //<Signup/>
+  //</div>
+  <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+  <div style={{ maxWidth: '600px', minWidth: '400px' }}>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route exact path="/" element={<Dashboard/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+        </Routes>
+      </AuthProvider>
+    </Router>
   </div>
+</Container>
+    
+    
+  
   );
 }
 
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,19 +4,19 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyDzj4EDpkhGfLfbR3WwAOcUzsBxeXlf7PA",
+  apiKey: "AIzaSyDzj4EDpkhGfLfbR3WwAOcUzsBxeXlf7PA", //process.env.REACT_APP_FIREBASE_KEY, //
 
-  authDomain: "illemhelykereso.firebaseapp.com",
+  authDomain: "illemhelykereso.firebaseapp.com", //process.env.REACT_APP_FIEBASE_AUTH_DOMAIN
 
-  projectId: "illemhelykereso",
+  projectId: "illemhelykereso", //process.env.REACT_APP_FIEBASE_PROJECT_ID,
 
-  storageBucket: "illemhelykereso.appspot.com",
+  storageBucket: "illemhelykereso.appspot.com", //process.env.REACT_APP_FIEBASE_STORAGE_BUCKET,
 
-  messagingSenderId: "440844589566",
+  messagingSenderId: "440844589566", //process.env.REACT_APP_FIEBASE_MESSAGING_SENDER_ID,
 
-  appId: "1:440844589566:web:f92923c5db585f48c648af",
+  appId: "1:440844589566:web:f92923c5db585f48c648af", //process.env.REACT_APP_FIEBASE_APP_ID,
 
-  measurementId: "G-JNBMYTVZSG"
+  measurementId: "G-JNBMYTVZSG" //process.env.REACT_APP_FIEBASE_MEASUREMENT_ID
 
 };
 
