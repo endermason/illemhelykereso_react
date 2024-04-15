a62680a493112a4da724363e13a7b0114b6987ee
Bence Szabó <theendermason@gmail.com>
2024-04-15 21:52:35 +0200

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import { Container } from "react-bootstrap";
-import "./App.css";
-import { Auth } from "./components/auth";
-import Signup from "./components/signup";
-import { AuthProvider } from "./contexts/AuthContext";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./components/dashboard";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-  //<div className="App">
-    //<Auth />
-    //<Signup/>
-  //</div>
-  <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-  <div style={{ maxWidth: '600px', minWidth: '400px' }}>
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route exact path="/" element={<Dashboard/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-        </Routes>
-      </AuthProvider>
-    </Router>
-  </div>
-</Container>
-    
-    
-  
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
